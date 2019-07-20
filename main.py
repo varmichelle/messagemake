067f08940df0132c784c7c8f1e863f089a232672
Katherine Huang <hello@katmh.com>
2019-07-20 15:04:55 -0700

    Rename background color changing to csscolors.html

--- main.py
+++ main.py
@@ -14,17 +14,6 @@ client = Client(account_sid, auth_token)
 @app.route('/', methods=['POST'])
 def index():
 	incomingMessageBody = request.values.get('Body', None)
-	incomingMessageSender = request.values.get('From')
-
-	# Reply with a thank you message
-	# Remove this for demo probably
-	message = client.messages \
-	.create(
-		body='Thanks! Your message has been received.',
-		from_='+12055572027',
-		to=incomingMessageSender
-	)
-	print(message.sid)
 
 	socketio.emit('message', incomingMessageBody)
 	return incomingMessageBody
