c1216193f89c4eeb55691e7788012efe9bb33a85
Katherine Huang <hello@katmh.com>
2019-07-20 13:15:13 -0700

    Add flask-socketio

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
+import os
+from twilio.rest import Client
+from flask_socketio import SocketIO
 from flask import Flask, request, render_template
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
+socketio = SocketIO(app)
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
 
 @app.route("/")
 def hello():
@@ -9,10 +17,22 @@ def hello():
 
 @app.route("/sms", methods=['GET', 'POST'])
 def sms():
-  # Get the message the user sent our Twilio number
-  body = request.values.get('Body', None)
-  print(body)
-  return body
+  if request.method == 'POST':
+    # Get the message the user sent our Twilio number
+    incomingMessageBody = request.values.get('Body', None)
+
+    # Reply with the same message
+    message = client.messages \
+    .create(
+        body=incomingMessageBody,
+        from_='+12055572027',
+        to='+19784831084'
+    )
+    print(message.sid)
+    return incomingMessageBody
+  
+  if request.method == 'GET':
+    return "sms page"
 
 if __name__ == "__main__":
-    app.run()
+  socketio.run(app)
