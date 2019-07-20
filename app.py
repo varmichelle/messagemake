92d832357b3f47f976478c14c2c8b17f8b2f9c15
Katherine Huang <hello@katmh.com>
2019-07-20 12:10:30 -0700

    Twilio API basics

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
   return "Hello World!"
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms():
+  # Get the message the user sent our Twilio number
+  body = request.values.get('Body', None)
+  print(body)
+  return body
 
 if __name__ == "__main__":
   app.run()
